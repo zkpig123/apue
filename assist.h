5a94245305b8923c856046337584e4536f2c2d66
zkpig123 <zkpig123@yahoo.com>
2017-02-16 22:11:42 +0800

    assist.h network.c

--- assist.h
+++ assist.h
@@ -1,3 +1,5 @@
+#ifndef _ASSIST_H_
+#define _ASSIST_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,9 +7,16 @@
 #include <sys/types.h>
 #include <grp.h>
 #include <errno.h>
+#include <netdb.h>
 #include <limits.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <ctype.h>
 
 void err (char *s);
 void p_err (char *s);
+#endif
---
+++ networks.c
@@ -0,0 +1,54 @@
+#include "assist.h"
+
+int get_addr_from_string (char *addr_str, uint32_t *addr)
+{
+	struct in_addr inaddr;
+	if (inet_aton(addr_str, &inaddr) == 0) return 1;
+	*addr = ntohl((uint32_t)inaddr.s_addr);
+	return 0;
+}
+
+void pr_network_item (struct netent *network_item)
+{
+	printf("network:\n");
+	printf("name:%s\n", network_item->n_name);
+	printf("alias list:\n");
+	for (char **list = network_item->n_aliases; *list != NULL; list++){
+		printf("           %s\n", *list);
+	}
+	printf("address type:%d\n", network_item->n_addrtype);
+	printf("network number:%d\n", network_item->n_net);
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2){
+		fprintf(stderr, "usage: progname network_addr.\n");
+		exit(1);
+	}
+	struct netent *network_item;
+	if (isdigit(argv[1][0])){
+		uint32_t addr;
+		if (get_addr_from_string(argv[1], &addr)){
+			fprintf(stderr, "invalid addr.\n");
+			exit(2);
+		}
+		if ((network_item = getnetbyaddr(addr, AF_INET)) == NULL){
+			fprintf(stderr, "getnetbyaddr failed.\n");
+			exit(3);
+		}
+	}
+	else{
+		if ((network_item = getnetbyname(argv[1])) == NULL){
+			fprintf(stderr, "getnetbyname failed.\n");
+			exit(4);
+		}
+	}
+
+	pr_network_item (network_item);
+	printf("\n");
+	printf("now traverse through /etc/networks\n");
+	setnetent(1);
+	while ((network_item = getnetent()) != NULL) pr_network_item (network_item);
+	endnetent();
+}
