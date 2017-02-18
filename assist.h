a72914d4b7b398bda76576b9b8be36144c3e441b
zkpig123 <zkpig123@yahoo.com>
2017-02-18 21:09:04 +0800

    assist.h

--- assist.h
+++ assist.h
@@ -1,5 +1,6 @@
 #ifndef _ASSIST_H_
 #define _ASSIST_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,6 +9,7 @@
 #include <grp.h>
 #include <errno.h>
 #include <netdb.h>
+#include <sys/utsname.h>
 #include <limits.h>
 #include <stdint.h>
 #include <inttypes.h>
@@ -16,7 +18,11 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/times.h>
+#include <sys/wait.h>
+#include <time.h>
 
-void err (char *s);
-void p_err (char *s);
+void err (const char *s);
+void p_err (const char *s);
+
 #endif
