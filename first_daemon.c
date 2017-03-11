41b651e18f48d7e48254963c58e6412e1770a94e
zkpig123 <zkpig123@yahoo.com>
2017-03-11 22:22:35 +0800

    home

--- first_daemon.c
+++ first_daemon.c
@@ -3,12 +3,22 @@
 int main (void)
 {
 	//change umask
+	if (umask(0) == -1) p_err("umask failed.\n");
 	
+	pid_t pid;
 	//fork one time, then setsid, then fork second time
+	if ((pid = fork()) == -1) p_err("fork1 failed.\n");
+	if (pid) return 0;
+	if (setsid() == -1) p_err("setsid failed.\n");
+	if ((pid = fork()) == -1) p_err("fork 2 failed.\n");
+	if (pid) return 0;
 	
 	//change current working directory
+	if (chdir("/") == -1) p_err("chdir to / failed.\n");
 	
+	int openmax;
 	//close descriptor
+#ifdef OPENMAX
 	
 	//redirect descriptor 0,1,2 to /dev/null
 }
